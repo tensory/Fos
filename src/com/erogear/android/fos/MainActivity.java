0e0ec30af047c40f972c5fab58d468289b48384a
Ari Lacenski <alacenski@gmail.com>
2013-10-11 13:37:10 -0700

    Simplest XML parser

--- src/com/erogear/android/fos/MainActivity.java
+++ src/com/erogear/android/fos/MainActivity.java
@@ -1,15 +1,64 @@
 package com.erogear.android.fos;
 
+import java.util.ArrayList;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import android.content.res.XmlResourceParser;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ListView;
 
 import com.actionbarsherlock.app.SherlockActivity;
-import com.erogear.android.fos.R;
 
 public class MainActivity extends SherlockActivity {
-
+	ListView lvAnimations;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		setupViews();
+	}
+	
+	protected void setupViews() {
+		lvAnimations = (ListView) findViewById(R.id.lvAnimations);
+		ArrayList<Preview> previews = new ArrayList<Preview>();
+		XmlResourceParser parser = getResources().getXml(R.xml.previews);
+		Preview p = null;
+		
+		try {
+			int event = parser.getEventType();
+			String TAG_PREVIEW = "preview";
+			
+			while (event != XmlPullParser.END_DOCUMENT) {
+				// et the type of the event  
+	            event = parser.getEventType();
+	       
+	            if (event == XmlPullParser.START_TAG) {
+	            	if (parser.getName().equals(TAG_PREVIEW)) {
+	            		p = new Preview();
+	            	}
+	            	if (parser.getName().equals("name")) {
+	            		p.setName(parser.getText());
+	            	}
+	            	
+	            } else if (event == XmlPullParser.END_TAG) {
+	            	if (parser.getName().equals(TAG_PREVIEW)) {
+	            		previews.add(p);
+	            	}
+	            }
+	            parser.next(); 
+			}
+		} catch (Exception e) {
+			Log.e("ERROR", e.getMessage());
+		} finally {
+			parser.close();
+		}
+		Log.d("PREVIEWS_CONTENTS", previews.toString());
+/*
+		PreviewAdapter prevAdapter = PreviewAdapter.getInstance();
+		prevAdapter.loadAll();
+		lvAnimations.setAdapter(prevAdapter);
+		*/
 	}
 }
---
+++ src/com/erogear/android/fos/Preview.java
@@ -0,0 +1,31 @@
+package com.erogear.android.fos;
+
+import java.util.ArrayList;
+
+import android.content.res.XmlResourceParser;
+import android.util.Log;
+
+public class Preview {
+	private String name;
+	
+	public void setName(String n) {
+		this.name = n;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public static ArrayList<Preview> getAll(XmlResourceParser res) {
+		ArrayList<Preview> prevs = new ArrayList<Preview>();
+		
+		try {
+			PreviewXmlParser prevXmlParser = new PreviewXmlParser();
+			prevs = (ArrayList<Preview>) prevXmlParser.parse(res);
+		} catch (Exception e) {
+			Log.e("FILE", "Not reading");
+			Log.e("FILE", e.getMessage());
+		}
+		return prevs;
+	}
+}
