7e972f7c48ae93523bcc2a5b5a7fb44ddbefda7d
Ari Lacenski <alacenski@gmail.com>
2013-10-10 23:09:23 -0700

    Changed package name

---
+++ src/com/erogear/android/fos/MainActivity.java
@@ -0,0 +1,15 @@
+package com.erogear.android.fos;
+
+import android.os.Bundle;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.erogear.android.fos.R;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
