f8ac4f5a6bfdc564d284ac4823d1a9ff490afb09
Ari Lacenski <alacenski@gmail.com>
2013-10-10 19:12:33 -0700

    Added ActionBarSherlock, dropped conflicting android-support-v4.jar

--- src/com/erogear/app/fos/MainActivity.java
+++ src/com/erogear/app/fos/MainActivity.java
@@ -1,22 +1,15 @@
 package com.erogear.app.fos;
 
 import android.os.Bundle;
-import android.app.Activity;
-import android.view.Menu;
 
-public class MainActivity extends Activity {
+import com.actionbarsherlock.app.SherlockActivity;
+
+public class MainActivity extends SherlockActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
 	}
 
 }
