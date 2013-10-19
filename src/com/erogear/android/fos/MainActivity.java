eef7e0ff97ffbe273039350dfec4aa2b46503170
Ari Lacenski <alacenski@gmail.com>
2013-10-18 18:47:24 -0700

    Cleaning up imports

--- src/com/erogear/android/fos/MainActivity.java
+++ src/com/erogear/android/fos/MainActivity.java
@@ -16,8 +16,6 @@ import android.os.IBinder;
 import android.os.Message;
 import android.util.Log;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
