81b9d5cd0f659130953558579742808ccf8c32d8
Ari Lacenski <alacenski@gmail.com>
2013-10-22 00:52:00 -0700

    Added service-starting capability

--- src/com/erogear/android/fos/MainActivity.java
+++ src/com/erogear/android/fos/MainActivity.java
@@ -1,11 +1,16 @@
 package com.erogear.android.fos;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.IBinder;
+import android.os.Message;
+import android.util.Log;
 import android.view.View;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
@@ -13,10 +18,15 @@ import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
 import com.erogear.android.bluetooth.comm.BluetoothVideoService;
+import com.erogear.android.bluetooth.comm.DeviceConnection;
 import com.erogear.android.fos.fragments.PreviewFragment;
 
 public class MainActivity extends SherlockFragmentActivity {
+	// Intent request codes
 	public static final int PREFERENCE_INTENT_RESULT = 1;
+	private static final int REQUEST_ENABLE_BT = 2;
+	
+	public static final String TAG = "MAIN";
 	public static final String PREVIEWS_DATA_TAG = "previews";
 	PreviewFragment list;
 	
@@ -50,10 +60,47 @@ public class MainActivity extends SherlockFragmentActivity {
 	@Override
 	protected void onResume() {
 		super.onResume();
+        Log.i(TAG, "--- ONRESUME ---");
 		
 		//Find or create the video service
         startService(new Intent(this, BluetoothVideoService.class));
-        svcConn = getBluetoothServiceConnection(); // Async method that sets up Bluetooth service
+        svcConn = new ServiceConnection() {
+			@Override
+			public void onServiceConnected(ComponentName name, IBinder service) {
+				// Set up notification, the old way.
+				// TODO add API check for using the new way
+				String appName = getString(R.string.app_name);
+				Notification note = new Notification(R.drawable.ic_launcher, appName, System.currentTimeMillis());
+				Intent i=new Intent(MainActivity.this, MainActivity.class);
+
+                i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP|
+                        Intent.FLAG_ACTIVITY_SINGLE_TOP);
+
+                PendingIntent pi = PendingIntent.getActivity(MainActivity.this, 0, i, 0);
+
+                note.setLatestEventInfo(MainActivity.this, appName, getString(R.string.eg_service_subtitle), pi);
+                note.flags|=Notification.FLAG_NO_CLEAR;
+                
+                // End notification setup
+                
+                BluetoothVideoService.MyBinder myBinder = (BluetoothVideoService.MyBinder) service;
+                videoSvc = myBinder.getService();
+                
+                Intent btOn = videoSvc.start(note); //If bluetooth is off, this returns an intent that will get it turned on
+               // videoSvc.addHandler(mHandler);
+                
+                if (btOn != null){
+                    startActivityForResult(btOn, REQUEST_ENABLE_BT);
+                }
+                
+                Log.d(TAG, "Bluetooth started");
+			}
+			
+			@Override
+			public void onServiceDisconnected(ComponentName name) {
+				 videoSvc.removeHandler(mHandler);
+			}
+        };
         // Events depending on service availability will be set in svcConn.onServiceConnected
         
         bindService(new Intent(this, BluetoothVideoService.class), svcConn, Service.START_STICKY);
@@ -84,17 +131,74 @@ public class MainActivity extends SherlockFragmentActivity {
 		}
 	}
 	
-	private ServiceConnection getBluetoothServiceConnection() {
-		return new ServiceConnection() {
-			@Override
-			public void onServiceConnected(ComponentName name, IBinder service) {
-				
-			}
-			
-			@Override
-			public void onServiceDisconnected(ComponentName name) {
-				
-			}
-		};
-	}
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.i(TAG, "--- ONSTOP ---");
+        if (svcConn != null) {
+        	unbindService(svcConn);
+        }
+    }
+	
+    // The Handler that gets information back from the BluetoothVideoService
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            DeviceConnection conn;
+/*
+            switch (msg.what) {
+            case BluetoothVideoService.MESSAGE_STATE_CHANGE:
+                switch(msg.arg1) {
+                    case BluetoothChatService.STATE_CONNECTION_LOST:
+                        conn = (DeviceConnection) msg.obj;
+                        Toast.makeText(VideoPlayer.this, "Lost connection to " + conn.getDeviceName(), Toast.LENGTH_SHORT).show();
+                        break;
+                    case BluetoothChatService.STATE_CONNECTION_FAIL:
+                        conn = (DeviceConnection) msg.obj;
+                        Toast.makeText(VideoPlayer.this, "Could not connect to device " + conn.getDeviceName(), Toast.LENGTH_SHORT).show();
+                        break;
+                }
+                break;
+            case BluetoothVideoService.MESSAGE_WRITE:
+                byte[] writeBuf = (byte[]) msg.obj;
+                addConversationLine("Me: " + byteArrayToHex(writeBuf));
+                break;
+            case BluetoothVideoService.MESSAGE_READ:
+                //byte[] readBuf = (byte[]) msg.obj;
+                //String readMessage = byteArrayToHex(readBuf);
+                addConversationLine(((FrameConsumer)msg.obj).getName() + ":  " + msg.arg1 + " " + msg.arg2);
+                break;
+            case BluetoothVideoService.MESSAGE_NEW_FRAME:
+                previewFrame((ByteBufferFrame)msg.obj);
+                mSeekBar.setProgress(msg.arg1);
+                mSeekBar.setMax(msg.arg2);
+                break;
+            case BluetoothVideoService.MESSAGE_VIDEO_LOADED:
+                mSeekBar.setMax(msg.arg1);
+                Toast.makeText(getApplicationContext(), "Video loaded!", Toast.LENGTH_SHORT).show();
+                addConversationLine((String) msg.obj);
+                break;
+            case BluetoothVideoService.MESSAGE_VIDEO_LOAD_FAIL:
+                Toast.makeText(getApplicationContext(), "Error while loading video; you may have some valid frames.", Toast.LENGTH_SHORT).show();
+                addConversationLine((String) msg.obj);
+                break;
+            case BluetoothVideoService.MESSAGE_FATAL_ERROR:
+                Toast.makeText(getApplicationContext(), "Video service had fatal error.", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+            case BluetoothVideoService.MESSAGE_NO_DEVICE:
+                conn = (DeviceConnection) msg.obj;
+                if(lastNoDeviceToast < System.currentTimeMillis() - 2000)
+                    Toast.makeText(getApplicationContext(), conn.getDeviceName() + " is not connected.", Toast.LENGTH_SHORT).show();
+
+                lastNoDeviceToast = System.currentTimeMillis();
+                break;
+            }
+            */
+        }
+    };
+
+    private void addConversationLine(String str) {
+    	Log.d("BluetoothServiceConnection", str);
+    }
 }
