4680f85d9a5a5b9c430911c187d5d33d9b3ba398
Ari Lacenski <alacenski@gmail.com>
2013-10-21 18:59:24 -0700

    Added svcConn function

--- src/com/erogear/android/fos/MainActivity.java
+++ src/com/erogear/android/fos/MainActivity.java
@@ -1,27 +1,64 @@
 package com.erogear.android.fos;
 
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
-import android.widget.Toast;
 
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
+import com.erogear.android.bluetooth.comm.BluetoothVideoService;
 import com.erogear.android.fos.fragments.PreviewFragment;
 
 public class MainActivity extends SherlockFragmentActivity {
-	public static int PREFERENCE_INTENT_RESULT = 1;
+	public static final int PREFERENCE_INTENT_RESULT = 1;
+	public static final String PREVIEWS_DATA_TAG = "previews";
 	PreviewFragment list;
+	
+	private BluetoothVideoService videoSvc;
+	private ServiceConnection svcConn;
+	
+	/*
+	private class LoadingTask extends AsyncTask<ArrayList<Preview>> {
+		@Override
+		protected void onPostExecute(ArrayList<Preview> result) {
+			Bundle previews = new Bundle();
+			previews.putParcelableArrayList(MainActivity.PREVIEWS_DATA_TAG, result);
+			list.setArguments(previews);
+			getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, list).commit();
+		}
+	}
+	*/
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		list = new PreviewFragment();
-        getSupportFragmentManager().beginTransaction().replace(R.id.frameLayout, list).commit();
+		
+		// Initialize previews
+		//new LoadingTask.execute();
+		
+		
 	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		
+		//Find or create the video service
+        startService(new Intent(this, BluetoothVideoService.class));
+        svcConn = getBluetoothServiceConnection(); // Async method that sets up Bluetooth service
+        // Events depending on service availability will be set in svcConn.onServiceConnected
+        
+        bindService(new Intent(this, BluetoothVideoService.class), svcConn, Service.START_STICKY);
+	}
+	
 	
 	public void onClickPreview(View v) {
 		list.onClickPreview(v);
@@ -45,5 +82,19 @@ public class MainActivity extends SherlockFragmentActivity {
 			default:
 				return super.onOptionsItemSelected(item);
 		}
+	}
+	
+	private ServiceConnection getBluetoothServiceConnection() {
+		return new ServiceConnection() {
+			@Override
+			public void onServiceConnected(ComponentName name, IBinder service) {
+				
+			}
+			
+			@Override
+			public void onServiceDisconnected(ComponentName name) {
+				
+			}
+		};
 	}
 }
