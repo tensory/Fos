3c366afe4bee5829b1674c989f61169a0435e9bc
Ari Lacenski <alacenski@gmail.com>
2013-10-23 17:21:36 -0700

    Deleted disused overrides

--- src/com/erogear/android/fos/fragments/PreviewFragment.java
+++ src/com/erogear/android/fos/fragments/PreviewFragment.java
@@ -19,53 +19,38 @@ public class PreviewFragment extends SherlockListFragment {
 	private ArrayList<Preview> previews;
 	private static int PREVIEW_NOT_SET_INDEX = -1;
 	private int selectedPreviewIndex;
-	
+
 	@Override
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
-		
+
 		selectedPreviewIndex = PreviewFragment.PREVIEW_NOT_SET_INDEX;
 		previews = getArguments().getParcelableArrayList(MainActivity.PREVIEWS_DATA_TAG);
 		setListAdapter(new PreviewAdapter(getActivity(), previews));
 	}
-	
+
 	@Override
-    public void onListItemClick(ListView l, View view, int position, long id) {
+	public void onListItemClick(ListView l, View view, int position, long id) {
 		int lastSelectedIndex = selectedPreviewIndex;
 		PreviewListItemLayoutView pvView = (PreviewListItemLayoutView) view;
 		if (lastSelectedIndex > PreviewFragment.PREVIEW_NOT_SET_INDEX) {
 			// A preview was selected before this event fired
-			
+
 			if (lastSelectedIndex != position) {
 				// New selection.
 				// Stop the animation on the last active preview, 
 				// and start it on the current one.
-				
+
 				PreviewListItemLayoutView oldView = (PreviewListItemLayoutView) l.getChildAt(lastSelectedIndex);
 				oldView.deactivate();
 			}
 		} 
 		pvView.toggleActive();
-		
+
 		selectedPreviewIndex = position;
-    }
-	
+	}
+
 	public void onClickPreview(View v) {
 		Log.d("CLICK_PREVIEW", "Currently looking at " + String.valueOf(selectedPreviewIndex));
 	}
-	/*
-	@Override
-	public void onDestroyView() {
-		super.onDestroyView();
-		
-		Log.e("PF", "View getting destroyed!");
-		Bundle loadedPreviews = new Bundle();
-		saveInstanceData(loadedPreviews);
-		
-	}
-	
-	protected saveInstanceData(Bundle bundle) {
-		bundle.putParcelableArrayList(MainActivity.PREVIEWS_DATA_TAG, value)
-	}
-	*/
 }
