78bcfd7bb95b17dd47d7d9f40dbefe69729ea2ce
Ari Lacenski <alacenski@gmail.com>
2013-10-29 09:28:25 -0700

    XML and dialog files.

---
+++ src/com/erogear/android/fos/NumberPickerDialogPreference.java
@@ -0,0 +1,17 @@
+package com.erogear.android.fos;
+
+import android.content.Context;
+import android.preference.DialogPreference;
+import android.util.AttributeSet;
+
+public class NumberPickerDialogPreference extends DialogPreference {
+
+	public NumberPickerDialogPreference(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setDialogLayoutResource(R.layout.preference_number_picker_dialog);
+		// TODO Auto-generated constructor stub
+		
+		// set layout   
+	}
+	
+}
