3ad57d05774f1b37b130f18f9b5bea898e5faab1
Ari Lacenski <alacenski@gmail.com>
2013-10-30 14:06:44 -0700

    Indent-a-thon

--- src/com/erogear/android/fos/fragments/PrefsFragment.java
+++ src/com/erogear/android/fos/fragments/PrefsFragment.java
@@ -14,44 +14,44 @@ public class PrefsFragment extends PreferenceFragment implements OnSharedPrefere
 	SharedPreferences sp;
 
 	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // Inflate preferences
-        addPreferencesFromResource(R.xml.preferences);
-        sp = getPreferenceManager().getSharedPreferences();
-        
-        String prefkeyFrameRate = getResources().getString(R.string.prefkeyFrameRate);
-        NumberPickerDialogPreference frameRate = (NumberPickerDialogPreference) findPreference(prefkeyFrameRate);
-        
-        // Set summary text
-        frameRate.setSummary(constructSummaryText(prefkeyFrameRate));
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		// Inflate preferences
+		addPreferencesFromResource(R.xml.preferences);
+		sp = getPreferenceManager().getSharedPreferences();
+
+		String prefkeyFrameRate = getResources().getString(R.string.prefkeyFrameRate);
+		NumberPickerDialogPreference frameRate = (NumberPickerDialogPreference) findPreference(prefkeyFrameRate);
+
+		// Set summary text
+		frameRate.setSummary(constructSummaryText(prefkeyFrameRate));
 	}
-	
-    
-    private String constructSummaryText(String key) {
-    	String summary = "";
-    	if (key.equals(getResources().getString(R.string.prefkeyFrameRate))) {
-    		// Preference is stored as a list index
-    		// Retrieve the list to show the user-readable value
-    		String[] values = getActivity().getResources().getStringArray(R.array.valuesFrameRate);
-    		int valueIndex = sp.getInt(key, 0);
-    		summary = String.valueOf(values[valueIndex]) + " " + getResources().getString(R.string.txtFramesPerSecond);
-    	}
-    	return summary;    	
-    }
 
-    @Override
-    public void onResume() {
-        super.onResume();
-        sp.registerOnSharedPreferenceChangeListener(this);
-    }
 
-    @Override
-    public void onPause() {
-        sp.unregisterOnSharedPreferenceChangeListener(this);
-        super.onPause();
-    }
+	private String constructSummaryText(String key) {
+		String summary = "";
+		if (key.equals(getResources().getString(R.string.prefkeyFrameRate))) {
+			// Preference is stored as a list index
+			// Retrieve the list to show the user-readable value
+			String[] values = getActivity().getResources().getStringArray(R.array.valuesFrameRate);
+			int valueIndex = sp.getInt(key, 0);
+			summary = String.valueOf(values[valueIndex]) + " " + getResources().getString(R.string.txtFramesPerSecond);
+		}
+		return summary;    	
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		sp.registerOnSharedPreferenceChangeListener(this);
+	}
+
+	@Override
+	public void onPause() {
+		sp.unregisterOnSharedPreferenceChangeListener(this);
+		super.onPause();
+	}
 
 	@Override
 	public void onSharedPreferenceChanged(SharedPreferences sharedPrefs, String key) {
