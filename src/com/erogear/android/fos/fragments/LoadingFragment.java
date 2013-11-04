43a246262451617b3c2ad98b7e05f83f5a387eb5
Ari Lacenski <alacenski@gmail.com>
2013-11-04 14:58:24 -0800

    Added fragment

---
+++ src/com/erogear/android/fos/fragments/LoadingFragment.java
@@ -0,0 +1,11 @@
+package com.erogear.android.fos.fragments;
+
+import com.actionbarsherlock.app.SherlockFragment;
+
+public class LoadingFragment extends SherlockFragment {
+
+	public LoadingFragment() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
