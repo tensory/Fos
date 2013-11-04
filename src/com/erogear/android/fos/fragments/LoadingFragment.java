b937f28b59158e90827c24a0d7895e0483a8cb71
Ari Lacenski <alacenski@gmail.com>
2013-11-04 15:05:34 -0800

    Adding loading state.

--- src/com/erogear/android/fos/fragments/LoadingFragment.java
+++ src/com/erogear/android/fos/fragments/LoadingFragment.java
@@ -1,6 +1,12 @@
 package com.erogear.android.fos.fragments;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import com.actionbarsherlock.app.SherlockFragment;
+import com.erogear.android.fos.R;
 
 public class LoadingFragment extends SherlockFragment {
 
@@ -8,4 +14,9 @@ public class LoadingFragment extends SherlockFragment {
 		// TODO Auto-generated constructor stub
 	}
 
+	@Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_loading, container, false);
+    }
 }
